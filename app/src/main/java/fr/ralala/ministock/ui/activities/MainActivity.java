d55a1af6453d9a06716b86e71e0f264555cc5f35
Keidan (K. Billonneau) <3134348+Keidan@users.noreply.github.com>
2025-11-18 21:32:13 +0100

    Fix backPressed

--- app/src/main/java/fr/ralala/ministock/ui/activities/MainActivity.java
+++ app/src/main/java/fr/ralala/ministock/ui/activities/MainActivity.java
@@ -14,6 +14,7 @@ import android.view.View;
 import android.widget.SearchView;
 import android.widget.Toast;
 
+import androidx.activity.OnBackPressedCallback;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -65,6 +66,13 @@ public class MainActivity extends AppCompatActivity implements SwipeEditDeleteRe
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    getOnBackPressedDispatcher().addCallback(this, new OnBackPressedCallback(true) {
+      @Override
+      public void handleOnBackPressed() {
+        back();
+      }
+    });
     mCollator = Collator.getInstance();
     mCollator.setStrength(Collator.PRIMARY);
     mApp = (ApplicationCtx) getApplication();
@@ -220,16 +228,11 @@ public class MainActivity extends AppCompatActivity implements SwipeEditDeleteRe
 
   /**
    * Called to handle the click on the back button.
-   *
-   * @deprecated For the moment I continue with this.
    */
-  @Deprecated
-  @Override
-  @SuppressWarnings("squid:S1133")
-  public void onBackPressed() {
+  public void back() {
     hideSearch();
     if (mLastBackPressed + BACK_TIME_DELAY > System.currentTimeMillis()) {
-      super.onBackPressed();
+      finish();
       return;
     } else {
       Toast.makeText(this, R.string.on_double_back_exit_text, Toast.LENGTH_SHORT).show();
